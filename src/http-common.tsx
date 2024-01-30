7effeffd5bd3ffc89cbfba88b4a66b0cec8c8fe4
vishnuanm <vishnu.annavarapu@in.abb.com>
2024-01-30 17:55:25 +0530

    code to run frontend and backend separately

--- src/http-common.tsx
+++ src/http-common.tsx
@@ -6,7 +6,7 @@ import axios, {
 } from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://0.0.0.0:5000",
   headers: {
     "Content-type": "application/json",
   },
