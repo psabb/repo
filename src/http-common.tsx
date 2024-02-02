6aaf3521f711c0a89edbad60d8ea41b957856b48
Santhosh_N <154316951+SanthuABB@users.noreply.github.com>
2024-02-02 13:08:09 +0530

    updated

--- src/http-common.tsx
+++ src/http-common.tsx
@@ -6,7 +6,7 @@ import axios, {
 } from "axios";
 
 const http = axios.create({
-  baseURL: "https://github-backend.azurewebsites.net/",
+  baseURL: "https://github-backend.azurewebsites.net",
   headers: {
     "Content-type": "application/json",
   },
