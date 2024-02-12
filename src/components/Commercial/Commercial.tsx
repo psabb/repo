5be038e4bb4aefeaadc50aede5b17b4329f1a622
vishnuanm <vishnu.annavarapu@in.abb.com>
2024-02-12 12:41:28 +0530

    Revert "Adding EntraID to the working deployment"
    This reverts commit fde60df9a86a752b9667ed937a821371ca06338a.

--- src/components/Commercial/Commercial.tsx
+++ src/components/Commercial/Commercial.tsx
@@ -4,7 +4,6 @@ import "./Commercial_NavbarContainer.css";
 import GlassMorphContainer from "./Commercial_GlassMorphContainer";
 import MySideNav from "./MySideNav";
 import localGif from "../../assets/abb.png";
-import { UserLogin } from "./UserLogin";
 
 interface NavbarContainerProps {}
 
@@ -18,9 +17,6 @@ const NavbarContainer: React.FC<NavbarContainerProps> = (props) => {
 
         <div className="logo-container">
           <img src={localGif} alt="Local GIF" className="logo-image" />
-        </div>
-        <div className="user-login-container">
-          <UserLogin {...props} />
         </div>
       </div>
       <GlassMorphContainer
--- src/msal/MsalConfig.js
+++
@@ -1,6 +0,0 @@
-export const msalConfig = {
-  authority:
-    "https://login.microsoftonline.com/372ee9e0-9ce0-4033-a64a-c07073a91ecd",
-  clientId: "abf3c63d-6e74-45bf-9e45-c8457a32c161",
-  redirectUri: document.getElementById("root").baseURI,
-};
