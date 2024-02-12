e136a44484ec85e92c0e6f20ed216d35f0dd540f
vishnuanm <vishnu.annavarapu@in.abb.com>
2024-02-12 14:06:03 +0530

    Reverting Back adding EntraID

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Commercial from "./components/Commercial/Commercial";
 import "./styles.css";
-import { GraphData } from "./components/Commercial/GraphData";
-import { withAuth } from "./msal/MsalAuthProvider";
 
-// Use ":" instead of "=" for type annotation
-const RootApp: React.FC = (props) => {
+const App: React.FC = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Commercial {...props} />} />
-          <Route path="/graph-data" element={<GraphData />} />
+          <Route path="/" element={<Commercial />} />
         </Routes>
       </Router>
     </>
   );
 };
 
-export const App = withAuth(RootApp); // Corrected: Pass RootApp instead of App
+export default App;
