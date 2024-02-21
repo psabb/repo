c222430886eff5e7ac609a9e51cecf262f7c8fda
SanthuABB <santhosh.n@in.abb.com>
2024-02-22 04:19:44 +0530

    updated hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Commercial from "./components/Commercial/Commercial";
 import "./styles.css";
 import LandingPage from "./components/Commercial/landingpage";
